854b4adfb6390361edff10a65c20949638b88b56
username <wecp-dummy@gmail.com>
2024-07-24 13:01:54 +0000

    front end day 17

--- frontend/src/app/bank/components/customersample/customersample.component.ts
+++ frontend/src/app/bank/components/customersample/customersample.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Customer } from '../../types/Customer';
-import { of } from 'rxjs';
-
+// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import { of } from 'rxjs';
+import { CustomerTS } from '../../types/tstypes/Customerts';
+ 
 @Component({
   selector: 'app-customersample',
   standalone: true,
   imports: [],
   templateUrl: './customersample.component.html',
-  styleUrl: './customersample.component.css'
+  styleUrls: ['./customersample.component.css']
 })
 export class CustomersampleComponent {
+  // customer: CustomerTS;
+ 
+ 
+    customer:CustomerTS = new CustomerTS("John Doe", "john@example.com", "john_doe", "password123", "User", "1");
+ 
+ 
+ 
  
 }
-
